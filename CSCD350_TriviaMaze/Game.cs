7a42408282c3f587ce8cb316035082c3fbebd856
Matthew Firmin <mfirmin@eagles.ewu.edu>
2019-11-04 09:21:11 -0800

    Added SQLinte connectio skeleton

--- CSCD350_TriviaMaze/Game.cs
+++ CSCD350_TriviaMaze/Game.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.IO;
+using System.Data;
+using System.Data.SQLite;
 
 namespace CSCD350_TriviaMaze
 {
@@ -11,6 +14,7 @@ namespace CSCD350_TriviaMaze
     {
         private Player player;
         private Maze maze;
+        private SQLiteConnection connection { get; set; }
 
         public Game()
         {
@@ -19,9 +23,25 @@ namespace CSCD350_TriviaMaze
 
         }
 
+        public void OpenSQLConnection()
+        {
+            SQLiteCommand command = null;
+            if (!(File.Exists(@"")))
+            {
+                SQLiteConnection.CreateFile(@"");
+                connection = new SQLiteConnection(@"");
+                connection.Open();
+                command = new SQLiteCommand(@"", connection);
+
+            }
+            command.ExecuteReader();
+
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Game Initialized...");
+            OpenSQLConnection();
             string name;
             Regex nameCheck = new Regex(@"^[A-Z]{1}[a-z]{1,15}, [A-Z]{1}[a-z]{1,15}, [A-Z](\.[A-Z]){0,2}$");
             do {
