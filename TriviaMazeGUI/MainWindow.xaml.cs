993de8d9a05647de26c2e915baff41ad9b023ab6
Matthew Firmin <mfirmin@eagles.ewu.edu>
2019-11-07 09:38:32 -0800

    Still a test

--- TriviaMazeGUI/MainWindow.xaml.cs
+++ TriviaMazeGUI/MainWindow.xaml.cs
@@ -12,15 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using System.Data.SQLite;
 
 namespace TriviaMazeGUI
 {
     public partial class MainWindow : Window
     {
+        //private SQLiteConnection connection { get; set; }
+        private RadioButton t;
+        private RadioButton f;
+        private About about;
         public MainWindow()
         {
             InitializeComponent();
-
+            TrueFalseQuestion();
             this.Loaded += MainWindow_Loaded;
         }
 
@@ -30,10 +35,61 @@ namespace TriviaMazeGUI
 
             UserPrompt userPromptWindow = new UserPrompt();
             userPromptWindow.Show();
-            
+
             //string name = userPromptWindow.NameEntry.Text;
             //MessageBox.Show(name);
         }
 
+        private void File_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mnuHelpAbout_Click(object sender, RoutedEventArgs e)
+        {
+            this.about = new About();
+            this.about.Show();
+
+        }
+
+        private void mnuInstructions_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TrueFalseQuestion()
+        {
+            t = new RadioButton();
+            f = new RadioButton();
+            StackPanel sp = new StackPanel();
+
+            Question.Children.Add(sp);
+            sp.Children.Add(t);
+            sp.Children.Add(f);
+
+            Canvas.SetTop(sp, 50);
+            Canvas.SetLeft(sp, 50);
+
+            t.Content = "True";
+            f.Content = "False";
+
+        }
+
+        private void btn_Submit_Click(object sender, RoutedEventArgs e)
+        {
+            Question.Children.Remove(t);
+            Question.Children.Remove(f);
+            Question.Children.Clear();
+        }
     }
 }
