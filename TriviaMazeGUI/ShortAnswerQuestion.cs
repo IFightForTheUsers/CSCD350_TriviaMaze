3602d812590bb4a779aa5f99d5e22389fe66d855
Steven Richmond <srichmond@eagles.ewu.edu>
2019-11-12 13:09:49 -0800

    Question classes to be wrapped.

---
+++ TriviaMazeGUI/ShortAnswerQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaMazeGUI
+{
+    class ShortAnswerQuestion : PanelQuestion
+    {
+        public ShortAnswerQuestion(Panel wrapping) : base(wrapping)
+        {
+        }
+
+        public new Room knock(Room from)
+        {
+            // stub to fill in with a question prompt
+            return base.knock(from);
+        }
+
+    }
+}
