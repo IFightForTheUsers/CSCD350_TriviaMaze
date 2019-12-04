860702c295b3167b30780ce60c69e982efde720e
Jeffrey Nix <jnix1@eagles.ewu.edu>
2019-12-04 11:26:42 -0800

    Quick add for API of save/load

---
+++ TriviaMazeGUI/SaveLoadManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaMazeGUI
+{
+    class SaveLoadManager
+    {
+        public void SaveClick()
+        {
+            // this will save the maze
+        }
+
+        public void LoadClick()
+        {
+            // this will load the maze
+        }
+    }
+}
