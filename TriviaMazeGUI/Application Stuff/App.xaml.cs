fdd2c336af33ebf8ddc1ea1bc5e5489d5a392dd7
Jeffrey Nix <jnix1@eagles.ewu.edu>
2019-12-04 15:36:59 -0800

    Clean up of project with folders to make it easier for me to find stuff

---
+++ TriviaMazeGUI/Application Stuff/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace TriviaMazeGUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Startup += App_Startup;
+        }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            TriviaMazeGUI.MainWindow.Instance.Show();
+        }
+    }
+}
